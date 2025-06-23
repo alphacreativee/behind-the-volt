e942f5beaf74161811935dc006dde70b5bc6acdd
HuuBang11 <huubang0312@gmail.com>
2025-06-23 16:42:45 +0700

    update code

--- assets/js/index/home.js
+++ assets/js/index/home.js
@@ -1,39 +1,10 @@
 import { preloadImages } from "../../libs/utils.js";
 
-function ourService() {
-  if ($(".our-services").length < 1) return;
-
-  const cards = document.querySelectorAll(".our-services .card");
-  cards.forEach((item) => {
-    item.addEventListener("mousemove", startRotate);
-    item.addEventListener("mouseout", stopRotate);
-  });
-
-  function startRotate(event) {
-    const cardItem = this.querySelector(".card-item");
-    const halfHeight = cardItem.offsetHeight / 2;
-    const halfWidth = cardItem.offsetWidth / 2;
-
-    cardItem.style.transform =
-      "rotateX(" +
-      -(event.offsetY - halfHeight) / 30 +
-      "deg) rotateY(" +
-      (event.offsetX - halfWidth) / 15 +
-      "deg)";
-  }
-
-  function stopRotate(event) {
-    const cardItem = this.querySelector(".card-item");
-    cardItem.style.transform = "rotate(0)";
-  }
-}
-
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
 };
 preloadImages("img").then(() => {
   init();
-  ourService();
 });
 $(window).on("beforeunload", function () {
   $(window).scrollTop(0);
